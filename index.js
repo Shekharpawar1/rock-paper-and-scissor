c6d81f0f2e7d084ebc119246fe4c26b0b4c3750b
Shekharpawar1 <86783159+Shekharpawar1@users.noreply.github.com>
2022-01-12 18:27:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let playerScore=0,
+    computerScore=0
+    playerSelection=prompt("enter your selection"),
+    computerSelection=computerPlay();
+
+function computerPlay(){
+    let gameSelection=["rock","paper","scissor"]
+    let compSelection=gameSelection[Math.floor(Math.random()*gameSelection.length)]
+    console.log(compSelection)
+}
+function gameRound(playerSelection,compSelection){
+    if(x)
+}
