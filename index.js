09c3e6b2f09cd9050bb916124d806baf91435fe8
Shekharpawar1 <86783159+Shekharpawar1@users.noreply.github.com>
2022-01-12 19:08:13 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-let playerScore=0,
-    computerScore=0
-    playerSelection=prompt("enter your selection"),
-    computerSelection=computerPlay();
-
 function computerPlay(){
-    let gameSelection=["rock","paper","scissor"]
-    let compSelection=gameSelection[Math.floor(Math.random()*gameSelection.length)]
-    console.log(compSelection)
+    let selection=["rock","paper","scissor"]
+    let compSelection=selection[Math.floor(Math.random()*selection.length)]
+    return compSelection
 }
-function gameRound(playerSelection,compSelection){
-    if(x)
+
+
+function gameRound(playerSelection,computerSelection){
+    if(playerSelection==="rock"){
+        if(computerSelection ==="rock") return "tie"
+        else if(computerSelection==="paper") return "computer won"
+        else if(computerSelection==="scissor") return "paper won"
+    }
+    else if(playerSelection==="paper"){
+        if(computerSelection ==="paper") return "tie"
+        else if(computerSelection==="scissor") return "computer won"
+        else if(computerSelection==="rock") return "paper won"
+    }
+    else if(playerSelection==="scissor"){
+        if(computerSelection ==="scissor") return "tie"
+        else if(computerSelection==="rock") return "computer won"
+        else if(computerSelection==="paper") return "paper won"
+    }
+    
 }
+
+
+
+
+
+
+
+
+let playerSelection=prompt("enter your selection")
+console.log(`player selection ${playerSelection} `)
+let computerSelection=computerPlay()
+
+console.log(gameRound(playerSelection,computerSelection))
